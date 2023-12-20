6879f0597b89b3d824a0e9c74cadc7373aab18d5
o0pmitev <pmitev89@gmail.com>
2023-12-20 18:44:03 +0200

    fix: add basic game logic

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,18 +7,51 @@ public class Target : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody targetRb;
 
-    void Start()
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 10.0f;
+    private float xRange = 4.0f;
+    private float yRange = -2.0f;
+    private float zRange = 0.0f;
+
+    private void Start()
     {
         targetRb = GetComponent<Rigidbody>();    
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange, zRange);
     }
 }
